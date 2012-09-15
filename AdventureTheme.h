d51fb02fe848f1a85245e5e085ea3de2a20e687e
Eliot <fadookie@gmail.com>
2012-09-15 08:49:43 -0700

    Remove unused data/code from example, move music to own header

---
+++ AdventureTheme.h
@@ -0,0 +1,29 @@
+int mIntro[] = {
+  //intro
+  NOTE_G5,
+  NOTE_D5,
+  NOTE_C5,
+  NOTE_D5,
+  //REST,
+  NOTE_G5,
+  NOTE_D5,
+  NOTE_C5,
+  NOTE_D5,
+  NOTE_G5
+  
+  };
+  
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  1,
+  1,
+  1,
+  1,
+  //1,
+  1,
+  1,
+  1,
+  1,
+  1
+  };
+
--- AdventureTime8Bit.ino
+++ AdventureTime8Bit.ino
@@ -1,62 +1,14 @@
 #include <TVout.h>
 #include <fontALL.h>
-#include "schematic.h"
-#include "TVOlogo.h"
 #include "Finn.h"
 #include "pitches.h"
+#include "AdventureTheme.h"
 
 TVout TV;
 
-int zOff = 150;
-int xOff = 0;
-int yOff = 0;
-int cSize = 50;
-int view_plane = 64;
-float angle = PI/60;
-const byte audioPin = 8;
-#define TONE(f, d) tone(audioPin, f, d)
-
-float cube3d[8][3] = {
-  {xOff - cSize,yOff + cSize,zOff - cSize},
-  {xOff + cSize,yOff + cSize,zOff - cSize},
-  {xOff - cSize,yOff - cSize,zOff - cSize},
-  {xOff + cSize,yOff - cSize,zOff - cSize},
-  {xOff - cSize,yOff + cSize,zOff + cSize},
-  {xOff + cSize,yOff + cSize,zOff + cSize},
-  {xOff - cSize,yOff - cSize,zOff + cSize},
-  {xOff + cSize,yOff - cSize,zOff + cSize}
-};
-unsigned char cube2d[8][2];
-
-
-int mIntro[] = {
-  //intro
-  NOTE_G5,
-  NOTE_D5,
-  NOTE_C5,
-  NOTE_D5,
-  //REST,
-  NOTE_G5,
-  NOTE_D5,
-  NOTE_C5,
-  NOTE_D5,
-  NOTE_G5
-  
-  };
-  
-  // note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  1,
-  1,
-  1,
-  1,
-  //1,
-  1,
-  1,
-  1,
-  1,
-  1
-  };
+void togglePixel(int x, int y) {
+  TV.set_pixel(x, y - 1, 2); //Toggle pixel. Messing with X since my image editor coordinates are off by one, boo
+}
 
 void finnWink() {
   togglePixel(32, 22); 
@@ -70,17 +22,12 @@ void finnTongue() {
   togglePixel(27, 29);
 }
 
-void togglePixel(int x, int y) {
-  TV.set_pixel(x, y - 1, 2); //Toggle pixel. Messing with X since my image editor coordinates are off by one, boo
-}
-
 void setup() {
   static bool hasSmiled = false;
   static bool finnHasWinked = false;
   bool loopMusic = false;
   TV.begin(NTSC,120,96);
   TV.select_font(font6x8);
-  //intro();
   TV.bitmap(0, 0, Finn);
   TV.delay(6000);
   TV.set_cursor(0, 65);
@@ -166,142 +113,7 @@ void setup() {
   TV.draw_line(10,10,110,86,INVERT);
   TV.draw_line(10,86,110,10,INVERT);
   TV.delay(2000);
-  
-  //random cube forever.
-  TV.clear_screen();
-  TV.print(16,40,"Random Cube");
-  TV.print(28,48,"Rotation");
-  TV.delay(2000);
-  
-  randomSeed(analogRead(0));
 }
 
 void loop() {
-  int rsteps = random(10,60);
-  switch(random(6)) {
-    case 0:
-      for (int i = 0; i < rsteps; i++) {
-        zrotate(angle);
-        printcube();
-      }
-      break;
-    case 1:
-      for (int i = 0; i < rsteps; i++) {
-        zrotate(2*PI - angle);
-        printcube();
-      }
-      break;
-    case 2:
-      for (int i = 0; i < rsteps; i++) {
-        xrotate(angle);
-        printcube();
-      }
-      break;
-    case 3:
-      for (int i = 0; i < rsteps; i++) {
-        xrotate(2*PI - angle);
-        printcube();
-      }
-      break;
-    case 4:
-      for (int i = 0; i < rsteps; i++) {
-        yrotate(angle);
-        printcube();
-      }
-      break;
-    case 5:
-      for (int i = 0; i < rsteps; i++) {
-        yrotate(2*PI - angle);
-        printcube();
-      }
-      break;
-  }
-}
-
-void intro() {
-unsigned char w,l,wb;
-  int index;
-  w = pgm_read_byte(TVOlogo);
-  l = pgm_read_byte(TVOlogo+1);
-  if (w&7)
-    wb = w/8 + 1;
-  else
-    wb = w/8;
-  index = wb*(l-1) + 2;
-  for ( unsigned char i = 1; i < l; i++ ) {
-    TV.bitmap((TV.hres() - w)/2,0,TVOlogo,index,w,i);
-    index-= wb;
-    TV.delay(50);
-  }
-  for (unsigned char i = 0; i < (TV.vres() - l)/2; i++) {
-    TV.bitmap((TV.hres() - w)/2,i,TVOlogo);
-    TV.delay(50);
-  }
-  TV.delay(3000);
-  TV.clear_screen();
-}
-
-void printcube() {
-  //calculate 2d points
-  for(byte i = 0; i < 8; i++) {
-    cube2d[i][0] = (unsigned char)((cube3d[i][0] * view_plane / cube3d[i][2]) + (TV.hres()/2));
-    cube2d[i][1] = (unsigned char)((cube3d[i][1] * view_plane / cube3d[i][2]) + (TV.vres()/2));
-  }
-  TV.delay_frame(1);
-  TV.clear_screen();
-  draw_cube();
-}
-
-void zrotate(float q) {
-  float tx,ty,temp;
-  for(byte i = 0; i < 8; i++) {
-    tx = cube3d[i][0] - xOff;
-    ty = cube3d[i][1] - yOff;
-    temp = tx * cos(q) - ty * sin(q);
-    ty = tx * sin(q) + ty * cos(q);
-    tx = temp;
-    cube3d[i][0] = tx + xOff;
-    cube3d[i][1] = ty + yOff;
-  }
-}
-
-void yrotate(float q) {
-  float tx,tz,temp;
-  for(byte i = 0; i < 8; i++) {
-    tx = cube3d[i][0] - xOff;
-    tz = cube3d[i][2] - zOff;
-    temp = tz * cos(q) - tx * sin(q);
-    tx = tz * sin(q) + tx * cos(q);
-    tz = temp;
-    cube3d[i][0] = tx + xOff;
-    cube3d[i][2] = tz + zOff;
-  }
-}
-
-void xrotate(float q) {
-  float ty,tz,temp;
-  for(byte i = 0; i < 8; i++) {
-    ty = cube3d[i][1] - yOff;
-    tz = cube3d[i][2] - zOff;
-    temp = ty * cos(q) - tz * sin(q);
-    tz = ty * sin(q) + tz * cos(q);
-    ty = temp;
-    cube3d[i][1] = ty + yOff;
-    cube3d[i][2] = tz + zOff;
-  }
-}
-
-void draw_cube() {
-  TV.draw_line(cube2d[0][0],cube2d[0][1],cube2d[1][0],cube2d[1][1],WHITE);
-  TV.draw_line(cube2d[0][0],cube2d[0][1],cube2d[2][0],cube2d[2][1],WHITE);
-  TV.draw_line(cube2d[0][0],cube2d[0][1],cube2d[4][0],cube2d[4][1],WHITE);
-  TV.draw_line(cube2d[1][0],cube2d[1][1],cube2d[5][0],cube2d[5][1],WHITE);
-  TV.draw_line(cube2d[1][0],cube2d[1][1],cube2d[3][0],cube2d[3][1],WHITE);
-  TV.draw_line(cube2d[2][0],cube2d[2][1],cube2d[6][0],cube2d[6][1],WHITE);
-  TV.draw_line(cube2d[2][0],cube2d[2][1],cube2d[3][0],cube2d[3][1],WHITE);
-  TV.draw_line(cube2d[4][0],cube2d[4][1],cube2d[6][0],cube2d[6][1],WHITE);
-  TV.draw_line(cube2d[4][0],cube2d[4][1],cube2d[5][0],cube2d[5][1],WHITE);
-  TV.draw_line(cube2d[7][0],cube2d[7][1],cube2d[6][0],cube2d[6][1],WHITE);
-  TV.draw_line(cube2d[7][0],cube2d[7][1],cube2d[3][0],cube2d[3][1],WHITE);
-  TV.draw_line(cube2d[7][0],cube2d[7][1],cube2d[5][0],cube2d[5][1],WHITE);
 }
